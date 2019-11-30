4b8cacdc342fbfcddf0b180656aaa9ada12e0475
unknown <z2466599860@163.com>
2019-11-30 11:05:41 +0800

    Vant完成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <div id="app">
-  1
-  </div>
+<div id="app">
+<van-button type="default">默认按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+</div>
 </template>
-
 <style lang="less">
-
 </style>
